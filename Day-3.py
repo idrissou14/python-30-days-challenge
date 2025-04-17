1475e386e361ef15f57cdf2d9b3aa54ae6ec9bda
idriss14 <idrissou02nang01@gmail.com>
2025-04-17 06:44:44 +0100

    update Day 3

--- Day-3.py
+++ Day-3.py
@@ -8,11 +8,12 @@ def calcul_age(annee):
 def main():
     print("############## Calculer un age grace a une annee de naissance ##########")
     annee_nais = int(input("Veillez saisire votre annee de naissance : "))
+    while(annee_nais >= datetime.date.today().year) :
+        annee_nais = int(input("Veillez saisire un annee de naissance valide : "))
     if datetime.date.today().year >= annee_nais :
         age = calcul_age(annee_nais)
         print(age)
-    else:
-        print("Veillez saisire une date valide")
+
 
 if __name__ == "__main__" :
     main()
