5bd1505b18857326f949100d49cf54af166d32f2
idriss14 <idrissou02nang01@gmail.com>
2025-04-18 23:41:44 +0100

    Day 5

---
+++ Day-5.py
@@ -0,0 +1,8 @@
+#Definir si un nombre est paire ou impaire
+
+numbre = int(input("entrer un nombre entier : "))
+
+if (numbre % 2) == 0 :
+    print("nomnre paire")
+else:
+    print("nombre impaire")
