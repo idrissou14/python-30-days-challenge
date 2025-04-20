a6929c242312061fe051239dd43397ef41b14db9
idriss14 <idrissou02nang01@gmail.com>
2025-04-20 21:59:14 +0100

    Day 7

---
+++ Day-7.py
@@ -0,0 +1,5 @@
+#convertir des minutes en heure,minute
+nombre = int(input("entre le nombre de minutes :"))
+heure = nombre//60
+minute = nombre%60
+print(f"{nombre} equivaut a {heure} heure(e) et {minute} minute(e)")
